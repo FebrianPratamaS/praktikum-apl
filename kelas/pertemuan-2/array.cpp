b6aa8eb8c8065296f8dc931c789909e867f93629
FebrianPratamaS <pratamasfebrian@gmail.com>
2025-03-11 14:25:11 +0800

    yolo

--- kelas/pertemuan-2/array.cpp
+++ kelas/pertemuan-2/array.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main() {
-    
+    int number[4] = {1,2,3,4};
+
+    int totalByte = sizeof(number);
+    int singleByte = sizeof(number[0]);
+
+    cout << totalByte << "\n" 
+    << singleByte;
+    return 0;
 }
